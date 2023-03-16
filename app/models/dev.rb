7d0db5b4703d8bcab1095e2812bf0895443fff35
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-15 21:13:52 -0400

    commit tree 38b2abf0509de9518f20ddbcd12263484bd41ade

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+has_many :freebies
+has_many :companies, through: :freebies
 
+def received_one(item_name)
+    self.freebies.find_by(item_name: item_name)
+    # returns false otherwise
+end
+
+def give_away(dev, freebie)
+freebie.dev = dev
+end
+# accepts a Dev instance and a Freebie instance, changes the freebie's dev to be the given dev; your code should only make the change if the freebie belongs to the dev who's giving it away # SE-B-Freebie-Tracker
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,20 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+comp1= Company.create(name: "Google", founding_year: 1998)
+comp2= Company.create(name: "Facebook", founding_year: 2004)
+comp3= Company.create(name: "Dunder Mifflin", founding_year: 2002)
+comp4= Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+dev1= Dev.create(name: "Rick")
+dev2= Dev.create(name: "Morty")
+dev3= Dev.create(name: "Mr. Meseeks")
+dev4= Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "T-Shirt", value: 10, dev_id: dev1.id, company_id: comp1.id)
+Freebie.create(item_name: "Blow Horn", value: 5, dev_id: dev2.id, company_id: comp2.id)
+Freebie.create(item_name: "Hat", value: 5, dev_id: dev3.id, company_id: comp3.id)
+Freebie.create(item_name: "Sticker", value: 1, dev_id: dev4.id, company_id: comp4.id)
 
 puts "Seeding done!"
